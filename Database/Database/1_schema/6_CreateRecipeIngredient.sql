e9fc78457df656babba6a2124a955f6edca5d83c
chuchko.al <chuchko.al@gmail.com>
2020-10-24 00:00:30 +0300

    Удален закомментированный код

--- Database/Database/1_schema/6_CreateRecipeIngredient.sql
+++ Database/Database/1_schema/6_CreateRecipeIngredient.sql
@@ -1,8 +1,6 @@
 --Создаем таблицу RecipeIngredient
 create table RecipeIngredient
 (
-	--id int not null identity(1,1) primary key,
-
 	RecipeId int foreign key(RecipeId) references Recipes(id) ON DELETE SET NULL,
 	IngredientId int foreign key(IngredientId) references Ingredients(id)
     primary key(RecipeId, IngredientId)
--- Database/Database/1_schema/7_CreateRecipeStep.sql
+++ Database/Database/1_schema/7_CreateRecipeStep.sql
@@ -1,7 +1,6 @@
 --Создаем таблицу RecipeStep
 create table RecipeStep
 (
-	--id int not null identity(1,1) primary key,
 	RecipeId int foreign key(RecipeId) references Recipes(id),
 	StepId int foreign key(StepId) references Steps(id)
 	primary key(RecipeId, StepId)
