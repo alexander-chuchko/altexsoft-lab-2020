c605089655d37259c58589048dd354582248a163
chuchko.al <chuchko.al@gmail.com>
2020-11-08 20:41:32 +0200

    Исправил  форму CTE на рекурсивную

--- Database/Database/3_queries/ChooseFrom3CategoriesFirstThreeRecipes.sql
+++ Database/Database/3_queries/ChooseFrom3CategoriesFirstThreeRecipes.sql
@@ -1,16 +1,17 @@
-WITH SelectedId(RecipesId) AS
+WITH RecursiveQuery(id, ParentId, Name) AS
 	(
-		select Recipes.Id
-		from Recipes, Categories
-		where Recipes.CategoryId=3 and  Recipes.CategoryId=Categories.Id and Categories.ParentId is null
-		union
-		select Recipes.Id
-		from Recipes, Categories
-		where Recipes.CategoryId=Categories.Id and Categories.ParentId =3
-	)
-SELECT Recipes.Name as NameRecept, Ingredients.Name as NameIngredient
-FROM Recipes 
-JOIN RecipeIngredient 
-ON RecipeIngredient.RecipeId = Recipes.Id 
-JOIN Ingredients on RecipeIngredient.IngredientId=Ingredients.Id
-WHERE Recipes.Id IN(SELECT TOP 3 RecipesId from SelectedId);
+		select categories.Id, categories.ParentId, categories.Name 
+		from Categories categories
+		where categories.Id=3
+		union all
+		select categories.Id, categories.ParentId, categories.Name 
+		from Categories categories
+		join RecursiveQuery rq on categories.ParentId=rq.Id
+	)		
+SELECT categories.Name as Category, recieps.Name as NameReciept, ingredient.Name as Ingredient 
+FROM  Recipes recieps
+JOIN RecipeIngredient ri ON recieps.Id = ri.RecipeId
+JOIN Categories categories ON recieps.CategoryId = categories.Id
+JOIN Ingredients ingredient ON ri.IngredientId = ingredient.Id
+WHERE recieps.Id IN (SELECT TOP 3 recieps1.Id FROM Recipes recieps1 INNER JOIN RecursiveQuery rq1 ON recieps1.CategoryId = rq1.Id)
+ORDER BY recieps.Name ASC;
